55a17302fbdbf6e11030e6022e4b885e1af5f43e
Antonio Mejia <avmejia@tec.mx>
2022-08-15 11:44:04 -0500

    clase 2.11

---
+++ 409-2-1-Basicos/basicos.py
@@ -0,0 +1,26 @@
+import math
+resultado = 1 + 2 ** 3 / 4 * 5
+print(resultado)
+
+tipo_dato = type(resultado)
+print(tipo_dato)
+
+numero_en_string = str(resultado)
+print(type(numero_en_string))
+print(numero_en_string)
+
+print(str(resultado))
+
+r = int("2")
+print(r)
+
+respuesta = input("Dame tu edad : ")
+respuesta = int(respuesta)
+print(type(respuesta))
+print(respuesta)
+
+pi = 3.14192
+print(f"LA edad de la persona es  {respuesta:.2f} y pi es {pi:.3f}")
+
+
+print(math.sqrt(4))
